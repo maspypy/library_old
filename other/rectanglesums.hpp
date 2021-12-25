f56af6b61dcd08a6f70cea4f4b78da754095a939
maspypy <maspypy.gmail.com>
2021-12-26 04:18:05 +0900

    improve small case

--- other/rectanglesums.hpp
+++ other/rectanglesums.hpp
@@ -1,11 +1,12 @@
 #include "ds/fenwick.hpp"
 
-template <typename WT = ll>
+template <typename WT = ll, bool SMALL = false>
 struct RectangleSums {
   int N;
   int n, Q;
   vi X, Y;
   vi keyX, keyY;
+  ll min_x, max_x, min_y, max_y;
   vc<WT> wt;
   vc<vc<pair<int, WT>>> add;
   vc<vc<tuple<int, int, int>>> query_l;
@@ -21,31 +22,54 @@ struct RectangleSums {
   }
 
   void compress() {
-    UNIQUE(keyX), UNIQUE(keyY);
-    add.resize(len(keyX) + 1);
-    query_l.resize(len(keyX) + 1);
-    query_r.resize(len(keyX) + 1);
-    FOR(i, N) {
-      ll x = X[i], y = Y[i], w = wt[i];
-      x = LB(keyX, x), y = LB(keyY, y);
-      add[x].eb(y, w);
+    if (!SMALL) {
+      UNIQUE(keyX), UNIQUE(keyY);
+      add.resize(len(keyX) + 1);
+      FOR(i, N) {
+        ll x = X[i], y = Y[i], w = wt[i];
+        x = LB(keyX, x), y = LB(keyY, y);
+        add[x].eb(y, w);
+      }
+    } else {
+      min_x = (N == 0 ? 0 : MIN(X));
+      max_x = (N == 0 ? 0 : MAX(X));
+      min_y = (N == 0 ? 0 : MIN(Y));
+      max_y = (N == 0 ? 0 : MAX(Y));
+      add.resize(max_x - min_x + 2);
+      FOR(i, N) {
+        ll x = X[i], y = Y[i], w = wt[i];
+        x -= min_x, y -= min_y;
+        add[x].eb(y, w);
+      }
     }
+    query_l.resize(len(add));
+    query_r.resize(len(add));
   }
 
   void add_rect(ll xl, ll xr, ll yl, ll yr) {
     assert(n == N);
-    xl = LB(keyX, xl), xr = LB(keyX, xr);
-    yl = LB(keyY, yl), yr = LB(keyY, yr);
+    if (!SMALL) {
+      xl = LB(keyX, xl), xr = LB(keyX, xr);
+      yl = LB(keyY, yl), yr = LB(keyY, yr);
+    } else {
+      xl -= min_x, xr -= min_x;
+      yl -= min_y, yr -= min_y;
+      xl = clamp(xl, 0LL, max_x - min_x + 1);
+      xr = clamp(xr, 0LL, max_x - min_x + 1);
+      yl = clamp(yl, 0LL, max_y - min_y + 1);
+      yr = clamp(yr, 0LL, max_y - min_y + 1);
+    }
     query_l[xl].eb(Q, yl, yr);
     query_r[xr].eb(Q, yl, yr);
     ++Q;
-  };
+  }
 
   vc<WT> calc() {
     assert(n == N);
     vc<WT> ANS(Q);
-    FenwickTree<WT> bit(len(keyY) + 1);
-    FOR(x, len(keyX) + 1) {
+    int k = (SMALL ? max_y - min_y + 2 : len(keyY) + 1);
+    FenwickTree<WT> bit(k);
+    FOR(x, len(add)) {
       FORIN(t, query_l[x]) {
         auto [q, yl, yr] = t;
         ANS[q] -= bit.sum(yl, yr);
@@ -58,7 +82,16 @@ struct RectangleSums {
         auto [y, w] = t;
         bit.add(y, w);
       }
+      query_l[x].clear();
+      query_r[x].clear();
     }
+    Q = 0;
     return ANS;
+  }
+
+  void doc() {
+    print("N 個の点は最初に決めてしまう。");
+    print("同じ点群に対してクエリをやり直せる。"); // abc233-h
+    print("SMALL=true にすると、座圧をしないため少し高速");
   }
 };
