939136e24276cbb6b04089f0b19c84c3a2c1bce9
maspypy <maspypy.gmail.com>
2021-12-27 05:46:56 +0900

    change lazysegtree to acl style

--- ds/lazysegtree.hpp
+++ ds/lazysegtree.hpp
@@ -1,115 +1,195 @@
 #pragma once
-template <typename T, typename OP, bool OP_commute = false>
+#include "my_template.hpp"
+
+#include "algebra/monoid.hpp"
+
+template <typename E, typename OP>
 struct LazySegTree {
-  using F = function<T(T, T)>;
-  using G = function<T(T, OP)>;
+  using F = function<E(E, E)>;
+  using G = function<E(E, OP)>;
   using H = function<OP(OP, OP)>;
-  int sz, n, height;
-  F f;
-  G g;
-  H h;
-  T ti;
-  OP ei;
-  vector<T> dat;
-  vector<OP> laz;
-  LazySegTree(F f, G g, H h, T ti, OP ei) : f(f), g(g), h(h), ti(ti), ei(ei) {}
+  int _n, size, log;
+  vc<E> dat;
+  vc<OP> laz;
+  F seg_f;
+  G seg_g;
+  H seg_h;
+  E unit;
+  OP OP_unit;
+  bool OP_commute;
 
-  void init(int n_) {
-    sz = n_;
-    n = 1;
-    height = 0;
-    while (n < n_) n <<= 1, height++;
-    dat.assign(2 * n, ti);
-    laz.assign(2 * n, ei);
+  LazySegTree(Monoid_OP<E, OP> Mono)
+      : seg_f(Mono.f)
+      , seg_g(Mono.g)
+      , seg_h(Mono.h)
+      , unit(Mono.unit)
+      , OP_unit(Mono.OP_unit)
+      , OP_commute(Mono.OP_commute) {}
+
+  void init(int n) {
+    _n = n;
+    log = 1;
+    while ((1 << log) < n) ++log;
+    size = 1 << log;
+    dat.assign(size << 1, unit);
+    laz.assign(size, OP_unit);
   }
 
-  void build(const vector<T> &v) {
-    int n_ = v.size();
-    init(n_);
-    for (int i = 0; i < n_; i++) dat[n + i] = v[i];
-    for (int i = n - 1; i; i--)
-      dat[i] = f(dat[(i << 1) | 0], dat[(i << 1) | 1]);
+  void build(const vector<E>& v) {
+    assert(v.size() == _n);
+    FOR(i, v.size()) { dat[size + i] = v[i]; }
+    FOR3_R(i, 1, size) { update(i); }
   }
 
-  inline T reflect(int k) { return laz[k] == ei ? dat[k] : g(dat[k], laz[k]); }
+  void update(int k) { dat[k] = seg_f(dat[2 * k], dat[2 * k + 1]); }
 
-  inline void propagate(int k) {
-    if (laz[k] == ei) return;
-    laz[(k << 1) | 0] = h(laz[(k << 1) | 0], laz[k]);
-    laz[(k << 1) | 1] = h(laz[(k << 1) | 1], laz[k]);
-    dat[k] = reflect(k);
-    laz[k] = ei;
+  void all_apply(int k, OP a) {
+    dat[k] = seg_g(dat[k], a);
+    if (k < size) laz[k] = seg_h(laz[k], a);
   }
 
-  inline void thrust(int k) {
-    for (int i = height; i; i--) propagate(k >> i);
+  void push(int k) {
+    all_apply(2 * k, laz[k]);
+    all_apply(2 * k + 1, laz[k]);
+    laz[k] = OP_unit;
   }
 
-  inline void recalc(int k) {
-    while (k >>= 1) dat[k] = f(reflect((k << 1) | 0), reflect((k << 1) | 1));
+  void set(int p, E x) {
+    assert(0 <= p && p < _n);
+    p += size;
+    for (int i = log; i >= 1; i--) push(p >> i);
+    dat[p] = x;
+    for (int i = 1; i <= log; i++) update(p >> i);
   }
 
-  void operate_range(int a, int b, OP x) {
-    if (a >= b) return;
-    a += n, b += n - 1;
-    if (!OP_commute) {
-      thrust(a);
-      thrust(b);
+  E get(int p) {
+    assert(0 <= p && p < _n);
+    p += size;
+    for (int i = log; i >= 1; i--) push(p >> i);
+    return dat[p];
+  }
+
+  E prod(int l, int r) {
+    assert(0 <= l && l <= r && r <= _n);
+    if (l == r) return unit;
+
+    l += size;
+    r += size;
+
+    for (int i = log; i >= 1; i--) {
+      if (((l >> i) << i) != l) push(l >> i);
+      if (((r >> i) << i) != r) push((r - 1) >> i);
     }
-    for (int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) laz[l] = h(laz[l], x), l++;
-      if (r & 1) --r, laz[r] = h(laz[r], x);
+
+    E sml = unit, smr = unit;
+    while (l < r) {
+      if (l & 1) sml = seg_f(sml, dat[l++]);
+      if (r & 1) smr = seg_f(dat[--r], smr);
+      l >>= 1;
+      r >>= 1;
     }
-    recalc(a);
-    recalc(b);
+
+    return seg_f(sml, smr);
   }
 
-  void set(int a, T x) {
-    thrust(a += n);
-    dat[a] = x;
-    laz[a] = ei;
-    recalc(a);
+  E all_prod() { return dat[1]; }
+
+  void apply(int p, OP a) {
+    assert(0 <= p && p < _n);
+    p += size;
+    if (!OP_commute)
+      for (int i = log; i >= 1; i--) push(p >> i);
+    dat[p] = seg_g(dat[p], a);
+    for (int i = 1; i <= log; i++) update(p >> i);
   }
 
-  T fold(int a, int b) {
-    if (a >= b) return ti;
-    thrust(a += n);
-    thrust(b += n - 1);
-    T vl = ti, vr = ti;
-    for (int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) vl = f(vl, reflect(l++));
-      if (r & 1) vr = f(reflect(--r), vr);
+  void apply(int l, int r, OP a) {
+    assert(0 <= l && l <= r && r <= _n);
+    if (l == r) return;
+
+    l += size;
+    r += size;
+
+    for (int i = log; i >= 1; i--) {
+      if (((l >> i) << i) != l) push(l >> i);
+      if (((r >> i) << i) != r) push((r - 1) >> i);
     }
-    return f(vl, vr);
+
+    {
+      int l2 = l, r2 = r;
+      while (l < r) {
+        if (l & 1) all_apply(l++, a);
+        if (r & 1) all_apply(--r, a);
+        l >>= 1;
+        r >>= 1;
+      }
+      l = l2;
+      r = r2;
+    }
+
+    for (int i = 1; i <= log; i++) {
+      if (((l >> i) << i) != l) update(l >> i);
+      if (((r >> i) << i) != r) update((r - 1) >> i);
+    }
   }
 
   template <typename C>
-  int find(int st, C &check, T &acc, int k, int l, int r) {
-    if (l + 1 == r) {
-      acc = f(acc, reflect(k));
-      return check(acc) ? k - n : -1;
-    }
-    propagate(k);
-    int m = (l + r) >> 1;
-    if (m <= st) return find(st, check, acc, (k << 1) | 1, m, r);
-    if (st <= l and !check(f(acc, dat[k]))) {
-      acc = f(acc, dat[k]);
-      return -1;
-    }
-    int vl = find(st, check, acc, (k << 1) | 0, l, m);
-    if (~vl) return vl;
-    return find(st, check, acc, (k << 1) | 1, m, r);
+  int max_right(C& check, int l) {
+    assert(0 <= l && l <= _n);
+    assert(check(unit));
+    if (l == _n) return _n;
+    l += size;
+    for (int i = log; i >= 1; i--) push(l >> i);
+    E sm = unit;
+    do {
+      while (l % 2 == 0) l >>= 1;
+      if (!check(seg_f(sm, dat[l]))) {
+        while (l < size) {
+          push(l);
+          l = (2 * l);
+          if (check(seg_f(sm, dat[l]))) {
+            sm = seg_f(sm, dat[l]);
+            l++;
+          }
+        }
+        return l - size;
+      }
+      sm = seg_f(sm, dat[l]);
+      l++;
+    } while ((l & -l) != l);
+    return _n;
   }
 
   template <typename C>
-  int find(int st, C &check) {
-    T acc = ti;
-    return find(st, check, acc, 1, 0, n);
+  int min_left(C& check, int r) {
+    assert(0 <= r && r <= _n);
+    assert(check(unit));
+    if (r == 0) return 0;
+    r += size;
+    for (int i = log; i >= 1; i--) push((r - 1) >> i);
+    E sm = unit;
+    do {
+      r--;
+      while (r > 1 && (r % 2)) r >>= 1;
+      if (!check(seg_f(dat[r], sm))) {
+        while (r < size) {
+          push(r);
+          r = (2 * r + 1);
+          if (check(seg_f(dat[r], sm))) {
+            sm = seg_f(dat[r], sm);
+            r--;
+          }
+        }
+        return r + 1 - size;
+      }
+      sm = seg_f(dat[r], sm);
+    } while ((r & -r) != r);
+    return 0;
   }
 
   void debug() {
-    vc<T> A(sz);
-    FOR(i, sz) A[i] = fold(i, i + 1);
-    print(A);
+    vc<E> v(_n);
+    FOR(i, _n) v[i] = get(i);
+    print("lazysegtree getall:", v);
   }
 };
