26e464245d370c7a605cb6e5f58831fcfb97caee
maspypy <maspypy.gmail.com>
2021-12-28 03:02:33 +0900

    add test

---
+++ test/library_checker/math/enumerate_primes.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/enumerate_primes"
+#include "my_template.hpp"
+
+#include "nt/primetable.hpp"
+
+void solve() {
+  const int LIM = 500'000'000;
+  auto [is_prime, primes] = primetable<LIM>();
+
+  LL(N, A, B);
+  int pi_N = UB(primes, N);
+
+  vc<int> ANS;
+  while (B < pi_N) {
+    ANS.eb(primes[B]);
+    B += A;
+  }
+  print(pi_N, len(ANS));
+  print(ANS);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  solve();
+
+  return 0;
+}
