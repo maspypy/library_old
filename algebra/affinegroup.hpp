97b00b6f74bb1675da9454622faa2b8e6cf41671
maspypy <maspypy.gmail.com>
2021-12-28 08:11:16 +0900

    add affine

---
+++ algebra/affinegroup.hpp
@@ -0,0 +1,15 @@
+template <typename K>
+struct AffineGroup {
+  using F = pair<K, K>;
+  using value_type = F;
+  static constexpr F op(const F &x, const F &y) noexcept {
+    return F({x.fi * y.fi, x.se * y.fi + y.se});
+  }
+  static constexpr F inverse(const F &x) {
+    auto [a, b] = x;
+    a = K(1) / a;
+    return {a, a * (-b)};
+  }
+  static constexpr K eval(const F &f, K x) noexcept { return f.fi * x + f.se; }
+  static constexpr F unit = {K(1), K(0)};
+};
