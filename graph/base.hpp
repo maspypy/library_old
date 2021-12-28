babc8c8318c3c6b09c77ace75c91571e6de5d076
maspypy <maspypy.gmail.com>
2021-12-29 02:24:31 +0900

    use csr in graph template

--- graph/base.hpp
+++ graph/base.hpp
@@ -1,49 +1,81 @@
 #pragma once
 
+// frm, to, cap, cost
 template <typename T>
-struct Edge {
-  int frm, to;
-  T cost;
-  int id;
-  Edge(int a, int b, T c, int d) : frm(a), to(b), cost(c), id(d) {}
-};
+using Edge = tuple<int, int, T, int>;
 
 template <typename T, bool directed = false>
 struct Graph {
   int N, M;
-  using value_type = T;
+  using cost_type = T;
   using edge_type = Edge<T>;
   vector<edge_type> edges;
-  vector<vector<edge_type>> G;
+  vector<int> indptr;
+  vector<edge_type> csr_edges;
+  bool prepared;
+
+  class OutgoingEdges {
+  public:
+    OutgoingEdges(const Graph* G, int l, int r) : G(G), l(l), r(r) {}
+
+    const edge_type* begin() const {
+      if (l == r) { return 0; }
+      return &G->csr_edges[l];
+    }
+
+    const edge_type* end() const {
+      if (l == r) { return 0; }
+      return &G->csr_edges[r];
+    }
+
+  private:
+    int l, r;
+    const Graph* G;
+  };
+
   Graph() {}
-  Graph(int N) : N(N), M(0), G(N), directed(bl) {}
+  Graph(int N) : N(N), M(0), prepared(0) {}
 
   void add(int frm, int to, T cost = 1, int i = -1) {
     if (i == -1) i = M;
-    auto e = edge_type(frm, to, cost, i);
+    auto e = edge_type({frm, to, cost, i});
     edges.eb(e);
-    G[frm].eb(e);
-    if (!directed) {
-      auto e_rev = edge_type(to, frm, cost, i);
-      G[to].eb(e_rev);
-    }
     ++M;
   }
 
-  void debug(bool detail = false) {
-    FOR(v, N) {
-      cout << v << " :";
-      for (auto e: G[v]) {
-        if (detail)
-          cout << " (" << e.frm << "," << e.to << "," << e.cost << "," << e.id
-               << ")";
-        else
-          cout << " " << e.to;
-      }
-      cout << "\n";
+  void prepare() {
+    assert(!prepared);
+    prepared = true;
+    indptr.assign(N + 1, 0);
+    for (auto&& [frm, to, cost, id]: edges) {
+      indptr[frm + 1]++;
+      if (!directed) indptr[to + 1]++;
+    }
+    FOR(v, N) indptr[v + 1] += indptr[v];
+    auto counter = indptr;
+    csr_edges.resize(indptr.back() + 1);
+    for (auto&& [frm, to, cost, id]: edges) {
+      csr_edges[counter[frm]++] = {frm, to, cost, id};
+      if (!directed) csr_edges[counter[to]++] = {to, frm, cost, id};
     }
   }
-  int size() { return N; }
 
-  vector<edge_type>& operator[](int v) { return G[v]; }
+  OutgoingEdges operator[](int v) const {
+    assert(prepared);
+    return {this, indptr[v], indptr[v + 1]};
+  }
+
+  void debug() {
+    print("Graph");
+    if (!prepared) {
+      print("frm to cost id");
+      for (auto&& e: edges) print(e);
+    } else {
+      print("indptr", indptr);
+      print("frm to cost id");
+      FOR(v, N) for (auto&& e: (*this)[v]) print(e);
+    }
+  }
+
+  int size() { return N; }
 };
