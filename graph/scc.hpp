3198e1108c49d36ff884aca2fb41a7b07fd4a15f
maspypy <maspypy.gmail.com>
2021-12-29 02:58:52 +0900

    add scc

--- graph/scc.hpp
+++ graph/scc.hpp
@@ -1,64 +1,55 @@
 #pragma once
 #include "graph/base.hpp"
 
-template <class T> struct SCC {
-  T &G;
-  ll N;
-  ll n_comp;
-  vc<int> comp_id;
+template <typename Graph>
+struct SCC {
+  Graph &G;
+  int N;
+  int cnt;
+  vc<int> comp;
   vc<int> low;
   vc<int> ord;
   vc<int> visited;
-  ll now = 0;
+  int now = 0;
 
-  SCC(T &G)
-      : G(G), N(G.N), n_comp(0), comp_id(G.N, 0), low(G.N, 0), ord(G.N, -1) {
-    assert(G.directed);
+  SCC(Graph &G)
+      : G(G), N(G.N), cnt(0), comp(G.N, 0), low(G.N, 0), ord(G.N, -1) {
+    assert(G.is_directed());
+    assert(G.is_prepared());
     build();
   }
 
-  ll operator[](ll v) { return comp_id[v]; }
+  int operator[](int v) { return comp[v]; }
 
   void dfs(int v) {
     low[v] = now;
     ord[v] = now;
     ++now;
     visited.eb(v);
-    for (auto e : G[v]) {
-      if (ord[e.to] == -1) {
-        dfs(e.to);
-        chmin(low[v], low[e.to]);
+    for (auto &&[frm, to, cost, id]: G[v]) {
+      if (ord[to] == -1) {
+        dfs(to);
+        chmin(low[v], low[to]);
       } else {
-        chmin(low[v], ord[e.to]);
+        chmin(low[v], ord[to]);
       }
     }
     if (low[v] == ord[v]) {
       while (1) {
-        ll u = visited.back();
+        int u = visited.back();
         visited.pop_back();
         ord[u] = N;
-        comp_id[u] = n_comp;
-        if (u == v)
-          break;
+        comp[u] = cnt;
+        if (u == v) break;
       }
-      ++n_comp;
+      ++cnt;
     }
   }
 
   void build() {
     FOR(v, N) {
-      if (ord[v] == -1)
-        dfs(v);
+      if (ord[v] == -1) dfs(v);
     }
-    FOR(v, N) comp_id[v] = n_comp - 1 - comp_id[v];
+    FOR(v, N) comp[v] = cnt - 1 - comp[v];
   }
-
-  vc<vc<int>> comp() {
-    if (n_comp == 0)
-      build();
-    vc<vc<int>> res;
-    res.resize(n_comp);
-    FOR(v, N) { res[comp_id[v]].eb(v); }
-    return res;
-  };
 };
---
+++ test/library_checker/graph/scc.test.cpp
@@ -0,0 +1,31 @@
+#include "my_template.hpp"
+
+#include "graph/base.hpp"
+#include "graph/scc.hpp"
+
+void solve() {
+  LL(N, M);
+  Graph<int, 1> G(N);
+  FOR(_, M) {
+    LL(a, b);
+    G.add(a, b);
+  }
+  G.prepare();
+
+  SCC scc(G);
+  auto C = scc.cnt;
+  vc<vc<int>> ANS(C);
+  FOR(v, N) ANS[scc[v]].eb(v);
+  print(len(ANS));
+  for (auto&& C: ANS) print(len(C), C);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  solve();
+
+  return 0;
+}
