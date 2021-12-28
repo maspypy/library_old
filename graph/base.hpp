b8cb26a8aafca74b484b52896190a07e2b646de9
maspypy <maspypy.gmail.com>
2021-12-29 00:40:28 +0900

    directed to template param

--- graph/base.hpp
+++ graph/base.hpp
@@ -8,23 +8,23 @@ struct Edge {
   Edge(int a, int b, T c, int d) : frm(a), to(b), cost(c), id(d) {}
 };
 
-template <typename T>
+template <typename T, bool directed = false>
 struct Graph {
   int N, M;
-  using edge_t = Edge<T>;
-  vector<edge_t> edges;
-  vector<vector<edge_t>> G;
-  bool directed;
+  using value_type = T;
+  using edge_type = Edge<T>;
+  vector<edge_type> edges;
+  vector<vector<edge_type>> G;
   Graph() {}
-  Graph(int N, bool bl = false) : N(N), M(0), G(N), directed(bl) {}
+  Graph(int N) : N(N), M(0), G(N), directed(bl) {}
 
   void add(int frm, int to, T cost = 1, int i = -1) {
     if (i == -1) i = M;
-    auto e = edge_t(frm, to, cost, i);
+    auto e = edge_type(frm, to, cost, i);
     edges.eb(e);
     G[frm].eb(e);
     if (!directed) {
-      auto e_rev = edge_t(to, frm, cost, i);
+      auto e_rev = edge_type(to, frm, cost, i);
       G[to].eb(e_rev);
     }
     ++M;
@@ -35,24 +35,15 @@ struct Graph {
       cout << v << " :";
       for (auto e: G[v]) {
         if (detail)
-          cout << " (" << e.frm << "," << e.to << "," << e.cost << "," << e.id << ")";
+          cout << " (" << e.frm << "," << e.to << "," << e.cost << "," << e.id
+               << ")";
         else
           cout << " " << e.to;
       }
       cout << "\n";
     }
   }
-
-  vector<int> degrees() {
-    vector<int> deg(N);
-    for (auto&& e: edges) {
-      deg[e.frm]++;
-      deg[e.to]++;
-    }
-    return deg;
-  }
-
   int size() { return N; }
 
-  vector<edge_t>& operator[](int v) { return G[v]; }
+  vector<edge_type>& operator[](int v) { return G[v]; }
 };
