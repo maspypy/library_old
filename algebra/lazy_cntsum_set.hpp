9a152c78100d39355bcda412c186e4ad49170b1f
maspypy <maspypy.gmail.com>
2022-01-01 23:44:54 +0900

    add verify

--- algebra/lazy_cntsum_set.hpp
+++ algebra/lazy_cntsum_set.hpp
@@ -1,10 +1,10 @@
 #include "algebra/group_cntsum.hpp"
 #include "algebra/monoid_set.hpp"
 
-template <typename E>
-struct Lazy_CntSum_Affine {
+template <typename E, E none_val = -1>
+struct Lazy_CntSum_Set {
   using X_structure = Group_CntSum<E>;
-  using A_structure = Monoid_Set<E>;
+  using A_structure = Monoid_Set<E, none_val>;
   using X = typename X_structure::value_type;
   using A = typename A_structure::value_type;
   static constexpr X act(const X &x, const A &a) {
