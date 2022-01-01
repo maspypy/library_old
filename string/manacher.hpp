cd9e299b8afb3ec17ec5b788741f2656fb813eb6
maspypy <maspypy.gmail.com>
2022-01-01 18:45:51 +0900

    verify manacher

--- string/manacher.hpp
+++ string/manacher.hpp
@@ -1,18 +1,30 @@
-vector<ll> manacher(string s) {
-  vector<ll> radius;
-  radius.resize(s.size());
+template <bool CALC_ALL, typename STRING>
+vector<int> longest_palindrome(STRING s) {
+  if (CALC_ALL) {
+    int n = len(s);
+    assert(n > 0);
+    s.resize(2 * n - 1);
+    FOR_R(i, n) s[2 * i] = s[i];
+    FOR(i, n - 1) s[2 * i + 1] = '-';
+  }
+  vector<int> A(len(s));
   int i = 0, j = 0;
-  while (i < s.size()) {
-    while (i - j >= 0 && i + j < s.size() && s[i - j] == s[i + j]) {
-      ++j;
-    }
-    radius[i] = j;
+  while (i < len(s)) {
+    while (i - j >= 0 && i + j < len(s) && s[i - j] == s[i + j]) ++j;
+    A[i] = j;
     int k = 1;
-    while (i - k >= 0 && i + k < s.size() && k + radius[i - k] < j) {
-      radius[i + k] = radius[i - k];
+    while (i - k >= 0 && i + k < len(s) && k + A[i - k] < j) {
+      A[i + k] = A[i - k];
       ++k;
     }
-    i += k;
-    j -= k;
+    i += k, j -= k;
   }
+  if (CALC_ALL) {
+    FOR(i, len(A)) {
+      if (!((i ^ A[i]) & 1)) A[i]--;
+    }
+  } else {
+    for (auto&& x: A) x = 2 * x - 1;
+  }
+  return A;
 }
