d926313b8b0f80b3d488091ae70b96da186d7c2f
maspypy <maspypy.gmail.com>
2022-01-03 15:09:12 +0900

    add bin search on tree

--- graph/treemonoid.hpp
+++ graph/treemonoid.hpp
@@ -48,6 +48,55 @@ struct TreeMonoid {
     return val;
   }
 
+  template <class F>
+  int find_path(F &check, int u, int v) {
+    /*
+    uv path 上で prod_path(u, x) が check を満たす最初の x
+    なければ -1
+    */
+    if (check(prod_path(u, u))) return u;
+    auto pd = hld.get_path_decomposition(u, v, edge);
+    X val = Monoid::unit;
+    for (auto &&[a, b]: pd) {
+      X x = (a <= b ? seg.prod(a, b + 1)
+                    : (Monoid::commute ? seg.prod(b, a + 1)
+                                       : seg_r.prod(b, a + 1)));
+      if (!check(Monoid::op(val, x))) {
+        val = Monoid::op(val, x);
+        continue;
+      }
+      auto check_tmp = [&](X x) -> bool { return !check(Monoid::op(val, x)); };
+      if (a <= b) {
+        auto i = seg.max_right(check_tmp, a);
+        return hld.LID[i];
+      } else {
+        auto i = (Monoid::commute ? seg.min_left(check_tmp, a + 1)
+                                  : seg_r.min_left(check_tmp, a + 1));
+        --i;
+        return hld.LID[i];
+      }
+    }
+    if (L == n) return n;
+    L += size;
+    X sm = Monoid::unit;
+    do {
+      while (L % 2 == 0) L >>= 1;
+      if (!check(Monoid::op(sm, dat[L]))) {
+        while (L < size) {
+          L = 2 * L;
+          if (check(Monoid::op(sm, dat[L]))) {
+            sm = Monoid::op(sm, dat[L]);
+            L++;
+          }
+        }
+        return L - size;
+      }
+      sm = Monoid::op(sm, dat[L]);
+      L++;
+    } while ((L & -L) != L);
+    return n;
+  }
+
   X prod_subtree(int u) {
     int l = hld.LID[u], r = hld.RID[u];
     return seg.prod(l + edge, r);
