6fc378824945928bef1e24c8c95d94c3a92a27f3
maspypy <maspypy.gmail.com>
2022-01-03 15:33:48 +0900

    add commute

--- algebra/monoid_min.hpp
+++ algebra/monoid_min.hpp
@@ -3,4 +3,5 @@ struct Monoid_Min {
   using value_type = X;
   static constexpr X op(const X &x, const X &y) noexcept { return min(x, y); }
   static constexpr X unit = INF;
+  static constexpr bool commute = true;
 };
