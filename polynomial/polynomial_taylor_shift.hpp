399d2c83f35160c190e018cfc4a1d4f32495cda9
maspypy <maspypy.gmail.com>
2022-01-07 04:48:54 +0900

    add polynomial taylor shift

---
+++ polynomial/polynomial_taylor_shift.hpp
@@ -0,0 +1,15 @@
+#include "polynomial/convolution.hpp"
+
+template <typename mint>
+vc<mint> polynomial_taylor_shift(vc<mint> a, mint c) {
+  ll N = len(a);
+  FOR(i, N) a[i] *= fact<mint>(i);
+  auto b = power_table<mint>(c, N);
+  FOR(i, N) b[i] *= fact_inv<mint>(i);
+  reverse(all(a));
+  auto f = convolution(a, b);
+  f.resize(N);
+  reverse(all(f));
+  FOR(i, N) f[i] *= fact_inv<mint>(i);
+  return f;
+}
---
+++ test/library_checker/polynomial/polynomial_taylor_shift.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/polynomial_taylor_shift"
+#include "my_template.hpp"
+
+#include "mod/modint.hpp"
+#include "polynomial/polynomial_taylor_shift.hpp"
+
+using mint = modint998;
+
+void solve() {
+  LL(N, c);
+  VEC(mint, A, N);
+  print(polynomial_taylor_shift(A, mint(c)));
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  solve();
+
+  return 0;
+}
