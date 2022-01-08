c7af6fe2478e249ab3b7d0a24ca778649e9467fa
maspypy <maspypy.gmail.com>
2022-01-08 14:13:21 +0900

    add exp

---
+++ test/library_checker/polynomial/exp_of_fps.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/exp_of_formal_power_series"
+#include "my_template.hpp"
+
+#include "polynomial/fps_exp.hpp"
+
+using mint = modint998;
+
+void solve() {
+  LL(N);
+  VEC(mint, A, N);
+  print(fps_exp(A));
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  solve();
+
+  return 0;
+}
