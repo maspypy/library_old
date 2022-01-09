1c90642e16ed659b0a86044bb2f3f6baffa4a315
maspypy <maspypy.gmail.com>
2022-01-10 08:22:16 +0900

    add sharp_p subset sum

--- test/library_checker/math/sharp_p_subset_sum.test.cpp
+++ test/library_checker/math/sharp_p_subset_sum.test.cpp
@@ -1,22 +1,12 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/sharp_p_subset_sum"
 #include "my_template.hpp"
-#include "polynomial/fps_exp.hpp"
+#include "other/sharp_p_subset_sum.hpp"
 
 using mint = modint998;
 void solve() {
   LL(N, T);
-  VEC(ll, S, N);
-  auto CNT = bin_count(S, T + 1);
-  vc<mint> logf(T + 1);
-  FOR3(x, 1, T + 1) {
-    FOR3(d, 1, T / x + 1) {
-      if (d & 1)
-        logf[d * x] += mint(CNT[x]) * inv<mint>(d);
-      else
-        logf[d * x] -= mint(CNT[x]) * inv<mint>(d);
-    }
-  }
-  auto f = fps_exp(logf);
+  VEC(int, S, N);
+  auto f = sharp_p_subset_sum<mint>(S, T+1);
   f.erase(f.begin());
   print(f);
 }
