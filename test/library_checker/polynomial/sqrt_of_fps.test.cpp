62a5a9048cbe34c2719f1bf01d9dd4b02bffee3a
maspypy <maspypy.gmail.com>
2022-01-09 17:44:52 +0900

    add fps sqrt

---
+++ test/library_checker/polynomial/sqrt_of_fps.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sqrt_of_formal_power_series"
+#include "my_template.hpp"
+#include "polynomial/fps_sqrt.hpp"
+
+using mint = modint998;
+void solve() {
+  LL(N);
+  VEC(mint, A, N);
+  auto B = fps_sqrt_any(A);
+  if (len(B) == N)
+    print(B);
+  else
+    print(-1);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  solve();
+
+  return 0;
+}
