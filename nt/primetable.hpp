672c669a28104e1827c56b002da42d1bd0a24e82
maspypy <maspypy.gmail.com>
2022-01-13 04:29:06 +0900

    use static

--- nt/primetable.hpp
+++ nt/primetable.hpp
@@ -2,6 +2,8 @@ template <int LIM = (1 << 20)>
 vc<int> primetable() {
   const int S = 32768, R = LIM / 2;
   static vc<int> primes = {2}, sieve(S + 1);
+  if(len(primes) > 1) return primes;  // already computed
+
   primes.reserve(int(LIM / log(LIM) * 1.1));
   vc<pi> cp;
   for (int i = 3; i <= S; i += 2) {
