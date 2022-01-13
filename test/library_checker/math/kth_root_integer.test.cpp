f1d40273a7b9b9143c92faa82b2655f24894a1ef
maspypy <maspypy.gmail.com>
2022-01-14 02:24:14 +0900

    kth root integer

---
+++ test/library_checker/math/kth_root_integer.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/kth_root_integer"
+#include "my_template.hpp"
+#include "other/integer_kth_root.hpp"
+
+void solve() {
+  LL(a, k);
+  print(integer_kth_root(k, a));
+}
+
+signed main() {
+  LL(T);
+  FOR_(T) solve();
+
+  return 0;
+}
