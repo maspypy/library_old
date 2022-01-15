74c6b44ab20407ec0eea792c425513bd719c390e
maspypy <maspypy.gmail.com>
2022-01-16 05:23:43 +0900

    fix

--- graph/degree.hpp
+++ graph/degree.hpp
@@ -2,14 +2,14 @@
 
 template <typename Graph>
 vector<int> degree(Graph& G) {
-  vector<int> deg(N);
-  FOR(v, N) deg[v] = len(G[v]);
+  vector<int> deg(G.N);
+  for(auto&& e : G.edges) deg[e.frm]++, deg[e.to]++;
   return deg;
 }
 
 template <typename Graph>
 pair<vector<int>, vector<int>> degree_inout(Graph& G) {
-  vector<int> indeg(N), outdeg(N);
+  vector<int> indeg(G.N), outdeg(G.N);
   for (auto&& e: G.edges) { indeg[e.to]++, outdeg[e.frm]++; }
   return {indeg, outdeg};
 }
