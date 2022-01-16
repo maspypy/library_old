00c5368ebcd23bc5f08549fa04315fd74328298c
maspypy <maspypy.gmail.com>
2022-01-16 11:46:43 +0900

    divisor zeta

---
+++ nt/zeta.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "nt/primetable.hpp"
+
+template <typename T>
+void divisor_zeta(vc<T>& A) {
+  int N = len(A) - 1;
+  auto& P = primetable(N);
+  for (auto&& p: P) { FOR3(x, 1, N / p + 1) A[p * x] += A[x]; }
+}
+
+template <typename T>
+void divisor_mobius(vc<T>& A) {
+  int N = len(A) - 1;
+  auto& P = primetable(N);
+  for (auto&& p: P) { FOR3_R(x, 1, N / p + 1) A[p * x] -= A[x]; }
+}
+
+template <typename T>
+void multiplier_zeta(vc<T>& A) {
+  int N = len(A) - 1;
+  auto& P = primetable(N);
+  for (auto&& p: P) { FOR3_R(x, 1, N / p + 1) A[x] += A[p * x]; }
+}
+
+template <typename T>
+void multiplier_mobius(vc<T>& A) {
+  int N = len(A) - 1;
+  auto& P = primetable(N);
+  for (auto&& p: P) { FOR3(x, 1, N / p + 1) A[p * x] -= A[x]; }
+}
