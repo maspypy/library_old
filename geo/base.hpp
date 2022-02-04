f87ea40469400514d27a9fee02cbd180d18550fc
maspypy <maspypy.gmail.com>
2022-02-04 11:52:12 +0900

    add constructor

--- geo/base.hpp
+++ geo/base.hpp
@@ -1,5 +1,10 @@
 struct Point {
   ll x, y;
+  Point(ll x=0, ll y=0) : x(x), y(y) {}
+
+  template <typename S, typename T>
+  Point(pair<S, T> p) : x(p.fi), y(p.se) {}
+
   Point operator-(Point p) const { return {x - p.x, y - p.y}; }
   ll det(Point p) const { return x * p.y - y * p.x; }
   ll dot(Point p) const { return x * p.x + y * p.y; }
