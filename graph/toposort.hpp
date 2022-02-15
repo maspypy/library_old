3b19154bd8603e5ab1eaabd6a43a1389ac0ec05d
maspypy <maspypy.gmail.com>
2022-02-15 15:17:39 +0900

    add toposort

---
+++ graph/toposort.hpp
@@ -0,0 +1,22 @@
+#include "graph/base.hpp"
+#include "graph/degree.hpp"
+
+template <typename Graph>
+vc<int> toposort(Graph& G) {
+  // DAG じゃなかったらエラー
+  assert(G.is_prepared());
+  assert(G.is_directed());
+  auto [indeg, outdeg] = degree_inout(G);
+  vc<int> V;
+  ll N = G.N;
+  FOR(v, N) if (indeg[v] == 0) V.eb(v);
+  ll p = 0;
+  while (p < len(V)) {
+    auto v = V[p++];
+    for (auto&& e: G[v]) {
+      if (--indeg[e.to] == 0) V.eb(e.to);
+    }
+  }
+  assert(len(V) == N);
+  return V;
+}
