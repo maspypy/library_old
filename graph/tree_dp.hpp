322d08b64c5ce49793dddea38fccc89745cdcae0
maspypy <maspypy.gmail.com>
2022-02-17 02:27:06 +0900

    add tree dp

--- graph/tree_dp.hpp
+++ graph/tree_dp.hpp
@@ -1,18 +1,33 @@
-template <typename E, typename F1, typename F2, typename F3, typename T>
-pair<vc<T>, vc<T>> tree_dp(E& ET, F1 f_ee, F2 f_ev, F3 f_ve, T unit) {
-  auto& V = ET.V;
-  auto& G = ET.G;
-  auto& par = ET.parent;
-  ll N = len(V);
-  vc<T> dp_v(N), dp_e(N);
-  FOR_R(i, N) {
-    auto v = V[i];
-    T dp = unit;
-    FORIN(e, G[v]) if (e.to != par[v]) {
-      dp_e[e.to] = f_ve(dp_v[e.to], e.id);
-      dp = f_ee(dp, dp_e[e.to]);
+// https://codeforces.com/contest/735/problem/E
+#include "graph/base.hpp"
+#include "graph/hld.hpp"
+
+template <typename Graph, typename Data, typename F1, typename F2, typename F3>
+vc<Data> tree_dp(Graph& G, F1 fee, F2 fev, F3 fve, Data unit) {
+  using E = typename Graph::edge_type;
+
+  int N = G.N;
+  HLD hld(G);
+  auto V = hld.V;
+  auto VR = V;
+  reverse(all(VR));
+  auto par = hld.parent;
+
+  vc<Data> dpv(N);
+  vc<Data> dpe1(N);
+
+  for (auto&& v: VR) {
+    auto val = unit;
+    E e0;
+    for (auto&& e: G[v]) {
+      if (e.to != par[v]) {
+        val = fee(val, dpe1[e.to]);
+      } else {
+        e0 = e;
+      }
     }
-    dp_v[v] = f_ev(dp, v);
+    dpv[v] = fev(val, v);
+    dpe1[v] = (v ? fve(dpv[v], e0) : unit);
   }
-  return {dp_v, dp_e};
+  return dpv;
 }
