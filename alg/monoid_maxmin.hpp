c517c2f90db61b45bc1352883c681803728095a5
maspypy <maspypy.gmail.com>
2022-03-07 11:07:16 +0900

    maxmin -> minmax

--- alg/monoid_maxmin.hpp
+++
@@ -1,8 +0,0 @@
-template <class X=long long, X INF=1LL<<60>
-struct Monoid_MaxMin {
-  using P = pair<X, X>;
-  using value_type = P;
-  static constexpr P op(const P x, const P y) noexcept { return {max(x.fi, y.fi), min(x.se, y.se)}; }
-  static constexpr P unit = P({-INF, INF});
-  static constexpr bool commute = true;
-};
