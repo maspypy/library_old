8f47a4933517d1c01a57f72df4ffe0f491025dce
maspypy <maspypy.gmail.com>
2022-03-14 00:14:24 +0900

    add multisource bfs

--- graph/bfs01.hpp
+++ graph/bfs01.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "graph/base.hpp"
 
-template<typename Graph>
+template <typename Graph>
 pair<vc<ll>, vc<int>> bfs01(Graph& G, ll v) {
   assert(G.is_prepared());
   int N = G.N;
@@ -14,7 +14,37 @@ pair<vc<ll>, vc<int>> bfs01(Graph& G, ll v) {
   while (!que.empty()) {
     auto v = que.front();
     que.pop_front();
-    for (auto&& e : G[v]) {
+    for (auto&& e: G[v]) {
+      if (dist[e.to] == -1 || dist[e.to] > dist[e.frm] + e.cost) {
+        dist[e.to] = dist[e.frm] + e.cost;
+        par[e.to] = e.frm;
+        if (e.cost == 0)
+          que.push_front(e.to);
+        else
+          que.push_back(e.to);
+      }
+    }
+  }
+  return {dist, par};
+}
+
+template <typename Graph>
+pair<vc<ll>, vc<int>> bfs01(Graph& G, vc<int> vs) {
+  assert(G.is_prepared());
+  int N = G.N;
+  vc<ll> dist(N, -1);
+  vc<int> par(N, -1);
+  deque<int> que;
+
+  for (auto&& v: vs) {
+    dist[v] = 0;
+    que.push_front(v);
+  }
+
+  while (!que.empty()) {
+    auto v = que.front();
+    que.pop_front();
+    for (auto&& e: G[v]) {
       if (dist[e.to] == -1 || dist[e.to] > dist[e.frm] + e.cost) {
         dist[e.to] = dist[e.frm] + e.cost;
         par[e.to] = e.frm;
