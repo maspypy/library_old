cf268907421f4aba35114f134bf21763bafcbc61
maspypy <maspypy.gmail.com>
2022-03-14 00:13:01 +0900

    add template

--- geo/base.hpp
+++ geo/base.hpp
@@ -1,13 +1,16 @@
+#pragma once
+
+template<typename X = long long>
 struct Point {
-  ll x, y;
-  Point(ll x=0, ll y=0) : x(x), y(y) {}
+  X x, y;
+  Point(X x=0, X y=0) : x(x), y(y) {}
 
   template <typename S, typename T>
   Point(pair<S, T> p) : x(p.fi), y(p.se) {}
 
   Point operator-(Point p) const { return {x - p.x, y - p.y}; }
-  ll det(Point p) const { return x * p.y - y * p.x; }
-  ll dot(Point p) const { return x * p.x + y * p.y; }
+  X det(Point p) const { return x * p.y - y * p.x; }
+  X dot(Point p) const { return x * p.x + y * p.y; }
   bool operator<(Point p) const {
     if (x != p.x) return x < p.x;
     return y < p.y;
