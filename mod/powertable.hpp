8d7938411cfc8398dd08d2a6451edf6707fe2482
maspypy <maspypy.gmail.com>
2022-03-19 16:17:22 +0900

    add comment

--- mod/powertable.hpp
+++ mod/powertable.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include "nt/primetable.hpp"
 
-template<typename mint>
+// a^0, ..., a^{N-1}
+template <typename mint>
 vc<mint> powertable_1(mint a, ll N) {
   // table of a^i
   vc<mint> f(N, 1);
@@ -9,19 +10,19 @@ vc<mint> powertable_1(mint a, ll N) {
   return f;
 }
 
-template<typename mint>
+// 0^e, ..., (N-1)^e
+template <typename mint>
 vc<mint> powertable_2(ll e, ll N) {
-  // table of i^e. N 以下の素数テーブルを利用する. 
   auto& primes = primetable(N);
   vc<mint> f(N, 1);
   f[0] = mint(0).pow(e);
-  for(auto&& p : primes){
-    if(p > N) break;
+  for (auto&& p: primes) {
+    if (p > N) break;
     mint xp = mint(p).pow(e);
     ll pp = p;
-    while(pp < N){
+    while (pp < N) {
       ll i = pp;
-      while(i < N){
+      while (i < N) {
         f[i] *= xp;
         i += pp;
       }
@@ -30,4 +31,3 @@ vc<mint> powertable_2(ll e, ll N) {
   }
   return f;
 }
-
