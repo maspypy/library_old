8d808120eee37f935fda45026a07a542474f14f8
maspypy <maspypy.gmail.com>
2022-04-11 04:11:46 +0900

    add polar

--- geo/base.hpp
+++ geo/base.hpp
@@ -8,9 +8,15 @@ struct Point {
   template <typename S, typename T>
   Point(pair<S, T> p) : x(p.fi), y(p.se) {}
 
+  Point operator+(Point p) const { return {x + p.x, y + p.y}; }
   Point operator-(Point p) const { return {x - p.x, y - p.y}; }
   X det(Point p) const { return x * p.y - y * p.x; }
   X dot(Point p) const { return x * p.x + y * p.y; }
+  pair<double,double> polar(){
+    double r = sqrt(x * x + y * y);
+    double theta = atan2(y, x);
+    return {r, theta};
+  }
   bool operator<(Point p) const {
     if (x != p.x) return x < p.x;
     return y < p.y;
