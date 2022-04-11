3475efd7af29543856a9279af273b8a54b2e8d22
maspypy <maspypy.gmail.com>
2022-04-11 13:18:01 +0900

    to 2d

--- ds/disjointsparse.hpp
+++ ds/disjointsparse.hpp
@@ -3,25 +3,20 @@ struct DisjointSparse {
   using X = typename Monoid::value_type;
   using value_type = X;
   int n, log;
-  vc<X> dat;
+  vc<vc<X>> dat;
 
-  DisjointSparse(const vc<X>& A) : n(len(A)) {
+  DisjointSparse(vc<X>& A) : n(len(A)) {
     log = 1;
     while ((1 << log) < n) ++log;
-    dat.reserve(log * n);
-    FOR(i, log) FOR(j, n) dat.eb(A[j]);
+    dat.assign(log, A);
 
     FOR(i, log) {
-      int s = n * i;
+      auto& v = dat[i];
       int b = 1 << i;
       for (int m = b; m <= n; m += 2 * b) {
         int L = m - b, R = min(n, m + b);
-        FOR3_R(j, L + 1, m) {
-          dat[s + j - 1] = Monoid::op(dat[s + j - 1], dat[s + j]);
-        }
-        FOR3(j, m, R - 1) {
-          dat[s + j + 1] = Monoid::op(dat[s + j], dat[s + j + 1]);
-        }
+        FOR3_R(j, L + 1, m) v[j - 1] = Monoid::op(v[j - 1], v[j]);
+        FOR3(j, m, R - 1) v[j + 1] = Monoid::op(v[j], v[j + 1]);
       }
     }
   }
@@ -29,16 +24,13 @@ struct DisjointSparse {
   X prod(int L, int R) {
     if (L == R) return Monoid::unit();
     --R;
-    if (L == R) return dat[L];
+    if (L == R) return dat[0][L];
     int k = 31 - __builtin_clz(L ^ R);
-    return Monoid::op(dat[n * k + L], dat[n * k + R]);
+    return Monoid::op(dat[k][L], dat[k][R]);
   }
 
   void debug() {
     print("disjoint sparse table");
-    FOR(i, log) {
-      vc<X> tmp = {dat.begin() + n * i, dat.begin() + n * (i + 1)};
-      print(tmp);
-    }
+    FOR(i, log) print(dat[i]);
   }
 };
