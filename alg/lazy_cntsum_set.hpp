b0f1c9036f0cbb67daa9edc4e92b3a954fd26823
maspypy <maspypy.gmail.com>
2022-04-11 18:49:27 +0900

    unit

--- alg/lazy_cntsum_set.hpp
+++ alg/lazy_cntsum_set.hpp
@@ -8,7 +8,7 @@ struct Lazy_CntSum_Set {
   using X = typename X_structure::value_type;
   using A = typename A_structure::value_type;
   static constexpr X act(const X &x, const A &a) {
-    if(a == A_structure::unit) return x;
+    if (a == A_structure::unit()) return x;
     return {x.fi, x.fi * a};
   }
 };
