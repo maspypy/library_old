e45edcac26d3f644ccda87a9cc830478b57b3f88
maspypy <maspypy.gmail.com>
2022-04-14 04:02:57 +0900

    add restore

--- mod/modular_subset_sum.hpp
+++ mod/modular_subset_sum.hpp
@@ -5,32 +5,35 @@
 /*
 (|vals| + mod) * log^2(mod)
 verify: https://codeforces.com/gym/103428/problem/C
+・vc<bool>：作れたかどうか
+・vc<int>：最後に使った idx
 */
-vc<bool> modular_subset_sum(int mod, vc<int> vals) {
+pair<vc<bool>, vc<int>> modular_subset_sum(int mod, vc<int> vals) {
   using Mono = Monoid_Rolling_Hash;
   RandomNumberGenerator RNG;
   const ll base = RNG(0, (1LL << 61) - 1);
-  vc<bool> A(mod + mod);
-  ll cnt = 0;
   vc<pair<modint61, modint61>> seg_raw(mod + mod);
   FOR(i, mod + mod) seg_raw[i] = {base, 0};
   SegTree<Mono> seg(seg_raw);
 
-  auto add = [&](ll x) -> void {
-    ++cnt;
-    A[x] = A[x + mod] = 1;
+  vc<bool> can(mod);
+  vc<int> idx(mod, -1);
+
+  auto add = [&](int x, int i) -> void {
+    can[x] = 1;
+    idx[x] = i;
     seg.set(x, {base, 1});
     seg.set(x + mod, {base, 1});
   };
 
-  add(0);
+  add(0, -1);
 
-  for (auto&& val: vals) {
+  FOR(i, len(vals)) {
+    ll val = vals[i];
     val %= mod;
-    if (cnt == mod) break;
     vc<pi> LR;
     LR.eb(0, mod);
-    vi ADD;
+    vc<int> ADD;
     while (len(LR)) {
       auto [L, R] = LR.back();
       LR.pop_back();
@@ -40,16 +43,15 @@ vc<bool> modular_subset_sum(int mod, vc<int> vals) {
       if (x1 == x2) continue;
       if (R == L + 1) {
         // 対称差 L が見つかった
-        if (!A[L]) ADD.eb(L);
+        if (!can[L]) ADD.eb(L);
         continue;
       }
       ll M = (L + R) / 2;
       LR.eb(L, M);
       LR.eb(M, R);
     }
-    for (auto&& a: ADD) add(a);
+    for (auto&& a: ADD) add(a, i);
   }
 
-  A.resize(mod);
-  return A;
+  return {can, idx};
 }
